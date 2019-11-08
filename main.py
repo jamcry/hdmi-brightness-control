ed60d8d7260b6bc897b31c3b14d8e03707d17488
Cem KIRAY <jamcry@hotmail.com>
2019-11-08 17:52:30 +0300

    Add improved monitor detection

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-##################################################################
+################################################################
 # This script is used for setting the brightness of HDMI output.
 # It executes xrandr command with given brightness value
 # Developed by JamCry - jamcry.xyz - 10/2019
@@ -8,8 +8,8 @@ import subprocess
 import sys
 
 # Returns the command and args as array (to use in subprocess.call)
-def get_args(brightness):
-    full_command = "xrandr --output HDMI-1-1 --brightness " + str(brightness)
+def get_args(brightness, output="HDMI-1-1"):
+    full_command = "xrandr --output " + output + " --brightness " + str(brightness)
     command_args = full_command.split(" ")
     return command_args
 
@@ -17,8 +17,15 @@ def get_args(brightness):
 def set_brightness(percent):
     percent_perten = percent / 100
     command_args = get_args(percent_perten)
-    subprocess.call(command_args)
-
+    while True:
+        try:
+            subprocess.check_output(command_args)
+            print(f"☑ SET BRIGHTNESS TO {percent}%.")
+            break
+        except subprocess.CalledProcessError as e:
+            print("☒ ERROR: Couldn't find the monitor. Trying again...\n...")
+            command_args = get_args(percent_perten, "HDMI-1")
+    
 # Set new brigtness value to cmdline argument if any, 100 otherwise
 new_brightness = int(sys.argv[1]) if (len(sys.argv) > 1) else 100
 
